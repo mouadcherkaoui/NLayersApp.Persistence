4e1cfc3d48cbcc0a58d6418d49cc1ce57a5496b9
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2019-11-11 17:53:21 +0100

    support load types from options parameter as assembly name and an array of types

---
+++ NLayersApp.Persistence.Tests/TestModel.cs
@@ -0,0 +1,34 @@
+﻿using NLayersApp.Persistence.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NLayersApp.Persistence.Tests
+{
+    public class TestModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Auditable : IAuditable
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class SoftDeletable : ISoftDelete
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class AuditableSoftDeletable : IAuditable, ISoftDelete
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- NLayersApp.Persistence/TypesResolver.cs
+++ NLayersApp.Persistence/TypesResolver.cs
@@ -1,21 +1,34 @@
+using Microsoft.Extensions.Options;
 using NLayersApp.Persistence.Abstractions;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace NLayersApp.Persistence
 {
     public class TypesResolver : ITypesResolver
     {
-        Func<string, Type> resolveAction; 
+        Func<string, Type> resolveAction;
+        public TypesResolver(IOptions<TypesResolverOptions> options)
+        {
+            var assembly = options.Value.Assembly;
+            var types = options.Value.Types;
+
+            RegisteredTypes = Assembly.LoadFrom($"{assembly}.dll")
+                .ExportedTypes
+                .Where(type => types.Any(t => type.Name.Equals(t, StringComparison.OrdinalIgnoreCase)))
+                .ToArray();
+        }
+
         public TypesResolver(Func<Type[]> registerAction)
         {
             RegisteredTypes = registerAction?.Invoke();
             resolveAction = (s) => RegisteredTypes.FirstOrDefault(t => t.Name == s); 
         }
         public Type[] RegisteredTypes { get ; }
-        public Type Resolve(string typeName) => this.asResolver().ResolveAction(typeName);
+        public Type Resolve(string typeName) => asResolver().ResolveAction(typeName);
         Func<string, Type> ITypesResolver.ResolveAction => resolveAction;
 
-        ITypesResolver asResolver() => ((ITypesResolver)this);
+        ITypesResolver asResolver() => this;
     }
 }
